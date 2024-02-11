6ee3020e314a491e3821e75828869b574ae01a4a
Sibgatt <sibgatullah765@gmail.com>
2024-02-11 23:23:34 +0600

    added a logo

--- src/components/layouts/Nav.jsx
+++ src/components/layouts/Nav.jsx
@@ -1,5 +1,7 @@
 import Container from "../Container"
 import Flex from "../Flex"
+import Image from "../Image"
+import Logo from '../../assets/logo.png'
 
 
 const Nav = () => {
@@ -7,9 +9,11 @@ const Nav = () => {
     <nav>
         <Container>
             <Flex>
-                <div className="w-1/4">asas</div>
-                <div className="w-1/2">asas</div>
-                <div className="w-1/4">asas</div>
+                <div className="w-1/4">
+                  <Image src={Logo}/>
+                </div>
+                <div className="w-1/2"></div>
+                <div className="w-1/4"></div>
             </Flex>
         </Container>
     </nav>
